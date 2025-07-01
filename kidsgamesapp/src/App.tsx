b998e1b0cd6fa9b98db169d39c5891b084fc667f
Feliziaboustedt <felizia.boustedt@hotmail.com>
2025-07-01 07:37:31 +0200

    Merge branch 'main' of https://github.com/feliziaboustedt/kidsgamesapp

--- kidsgamesapp/src/App.tsx
+++ kidsgamesapp/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { RouterProvider } from "react-router";
+import { router } from "./Router";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
