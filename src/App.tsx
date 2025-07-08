6c30ed01cdd37221a44a5f9113e4f50b2c8f0cec
Feliziaboustedt <felizia.boustedt@hotmail.com>
2025-07-08 09:09:38 +0200

    Deploy update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import { RouterProvider } from "react-router";
 import { router } from "./Router";
-import { BrowserRouter } from "react-router";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/kidsgamesapp">
-        <RouterProvider router={router}></RouterProvider>
-      </BrowserRouter>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
